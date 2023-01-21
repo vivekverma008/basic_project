bffc4eb0f8d04d7de1ccca66776570d3ad4b645f
vivekverma008 <vibhuvivekverma@gmail.com>
2023-01-22 01:04:44 +0530

    view engine ejs added in package.json

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const usersHandler = require('../controller/user_handler');
 
-console.log('users called');
+
 router.get('/profile',usersHandler.profile);
 router.get('/posts',usersHandler.posts);
 
